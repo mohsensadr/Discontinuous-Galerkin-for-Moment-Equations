c46a4e6f724910e15b74ae98aeceda0e632600c8
Mohsen Sadr <sadr@turbine.mathcces.rwth-aachen.de>
2016-09-20 18:31:27 +0200

    setting the path is added

---
+++ setting_paths.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$HOME/programs/gcc-6.1.0/lib64/:/home/sadr/programs/pardiso_swiss:/home/sadr/programs/mpc-0.8.1/lib:$LD_LIBRARY_PATH
+export PATH=$HOME/programs/gcc-6.1.0/bin:$PATH
